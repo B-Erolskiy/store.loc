c3a2a71f6dc36a0c87a5c5d57f2fa9dcbab9c679
Bogdan Erolskiy <59888566+B-Erolskiy@users.noreply.github.com>
2020-05-20 23:00:02 +0300

    working on the admin panel

--- modules/admin/controllers/AppAdminController.php
+++ modules/admin/controllers/AppAdminController.php
@@ -3,14 +3,27 @@
 
 namespace app\modules\admin\controllers;
 use yii\filters\AccessControl;
-
-
 use app\controllers\AppController;
+
+
 
 class AppAdminController extends AppController
 {
     public function behaviors()
     {
-        return parent::behaviors();
+        //разрешение доступа к actions данного контроллера
+        //только зарегистрированным пользователям
+
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
     }
 }

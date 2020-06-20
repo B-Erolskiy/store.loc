284ae4a5ad518c50d866164d52408d650fd99414
Bogdan Erolskiy <59888566+B-Erolskiy@users.noreply.github.com>
2020-06-20 11:20:27 +0300

    Fix title

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends AppController
             throw new \yii\web\HttpException(404, 'Такого товара не существует');
 
         $hits = Product::find()->where(['hit' => 1])->limit(6)->all();
-        $this->setMeta('Erolsky Store | '. $product->name, $product->keywords_tag, $product->description_tag);
+        $this->setMeta('TMART | '. $product->name, $product->keywords_tag, $product->description_tag);
         //$product = Product::find()->with('category')->where(['id' => $id])->limit(1)->one();
         return $this->render('view', compact('product', 'hits'));
     }

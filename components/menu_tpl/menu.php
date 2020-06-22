9d6d0a39ee2089f294c62246dec3cdb70b77caa2
Bogdan Erolskiy <59888566+B-Erolskiy@users.noreply.github.com>
2020-06-22 11:28:02 +0300

    Category menu edit

--- components/menu_tpl/menu.php
+++ components/menu_tpl/menu.php
@@ -4,26 +4,29 @@
             <a href="<?= \yii\helpers\Url::to(['category/view', 'id' => $category['alias']])?>"><?= $category['name']?><i class="zmdi zmdi-chevron-right"></i></a>
             <div class="category-menu-dropdown">
                 <?php foreach ($category['childs'] as $categoryChildFirst):?>
+                    <?php if(isset($categoryChildFirst['childs'])) :?>
                     <div class="category-common mb--30">
                         <h4>
                             <a class="categories-subtitle" href="<?= \yii\helpers\Url::to(['category/view', 'id' => $categoryChildFirst['alias']])?>">
                                 <?= $categoryChildFirst['name']?></a>
                         </h4>
                         <ul class="block">
-                            <?php if(isset($categoryChildFirst['childs'])) :?>
                                 <?php foreach ($categoryChildFirst['childs'] as $categoryChildSec):?>
                                     <li>
                                         <a href="<?= \yii\helpers\Url::to(['category/view', 'id' => $categoryChildSec['alias']])?>">
                                             <?= $categoryChildSec['name']?></a>
                                     </li>
                                 <?php endforeach;?>
-                            <?php endif;?>
                         </ul>
                     </div>
-                <?php endforeach;?>
+                <?php else :?>
+                        <a style="border-bottom: 0px" href="<?= \yii\helpers\Url::to(['category/view', 'id' => $categoryChildFirst['alias']])?>">
+                            <?= $categoryChildFirst['name']?></a>
+                <?php endif;?>
+        <?php endforeach;?>
             </div>
         </li>
-    <?php else:?>
+    <?php else :?>
         <li>
             <a href="<?= \yii\helpers\Url::to(['category/view', 'id' => $category['alias']])?>">
                 <?= $category['name']?></a>
